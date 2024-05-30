65aa96588749c077d9e5fcde45a26ff2e4a39cea
Daniels Ciemiņš <ciemins.daniels@gmail.com>
2024-05-30 20:14:19 +0300

    Make basic TODOItem class

--- app/TODOItem.php
+++ app/TODOItem.php
@@ -4,5 +4,34 @@ namespace App;
 
 class TODOItem
 {
+    const STATUS_UNCHECKED = "unchecked";
+    const STATUS_CHECKED = "checked";
 
+    private string $text;
+    private string $state = self::STATUS_UNCHECKED;
+
+    public function __construct(string $text)
+    {
+        $this->text = $text;
+    }
+
+    public function text(): string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): void
+    {
+        $this->text = $text;
+    }
+
+    public function state(): string
+    {
+        return $this->state;
+    }
+
+    public function setState(string $state): void
+    {
+        $this->state = $state;
+    }
 }
